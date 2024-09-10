df8a0ced45df5f5e088b954b848705bef8280d3c
Sium39 <siumbinnoor@gmail.com>
2024-09-10 23:42:33 +0900

    important arithmetic operations

---
+++ arithmetic_operation.py
@@ -0,0 +1,15 @@
+# augmented assigned operator
+
+x = 10
+x += 5
+print(x)
+
+# operator precedence (**, *,/, +,-)
+
+y = 10 + 3 * 4 ** 2
+print(y)
+
+#parenthesis 
+
+z = (10 - 5) * 2 ** 3
+print(z)
