b8c1c953b1cf60bcffc5a291d60e1ae2ef4a5cf5
Sium39 <siumbinnoor@gmail.com>
2024-09-10 16:47:08 +0900

    all about strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+#single quote and double quotes in strings
+
+# data = "Python's course for beginners"
+# print(data)
+
+# data2 = 'Python "Course" for beginners'
+# print(data2)
+
+name = 'Jennifer'
+print(name[1:-1])
