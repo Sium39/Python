eb31314b809c3540ecbac759e0a6fe62e71179d3
Sium39 <siumbinnoor@gmail.com>
2024-09-10 16:29:01 +0900

    type conversion

---
+++ type_conversion.py
@@ -0,0 +1,8 @@
+birth = input('Enter your Birth year: ')
+age = 2024 - int(birth)
+print('Your age is: ',age)
+
+#task (pound to kg)
+weight = input('Enter your weight in pound: ')
+kg =int(weight) * 0.45
+print('You are ', kg, ' kg')
