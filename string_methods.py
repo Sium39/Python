250c14416cd1329bc6a1034fa02db1861628dfd4
Sium39 <siumbinnoor@gmail.com>
2024-09-10 23:26:07 +0900

    some strings methods like upper, lower, find, replace, in

---
+++ string_methods.py
@@ -0,0 +1,9 @@
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('B'))
+print(course.replace('Beginners','Professional'))
+print('Python' in course)
+
+#find is used for searching index and in is given as boolien expression
