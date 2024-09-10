0124aae8f5bb29e990a3e7f67e2128c2949b2650
Sium39 <siumbinnoor@gmail.com>
2024-09-10 23:48:34 +0900

    arithmetic operation

--- arithmetic_operation.py
+++ arithmetic_operation.py
@@ -1,15 +1,12 @@
 # augmented assigned operator
-
 x = 10
 x += 5
 print(x)
 
 # operator precedence (**, *,/, +,-)
-
 y = 10 + 3 * 4 ** 2
 print(y)
 
 #parenthesis 
-
-z = (10 - 5) * 2 ** 3
+z = (10 - 5) * 5 - 3
 print(z)
