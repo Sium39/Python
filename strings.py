6fa747fe12be268e25460de1374bc7cf28171941
Sium39 <siumbinnoor@gmail.com>
2024-09-10 16:56:27 +0900

    string added with brackets

--- strings.py
+++ strings.py
@@ -1,10 +1,20 @@
 #single quote and double quotes in strings
 
-# data = "Python's course for beginners"
-# print(data)
+data = "Python's course for beginners"
+print(data)
 
-# data2 = 'Python "Course" for beginners'
-# print(data2)
+data2 = 'Python "Course" for beginners'
+print(data2)
 
+#task
 name = 'Jennifer'
 print(name[1:-1])
+
+#task-2 (use middle name with a [middle name] bracket)
+f_name = 'Sium'
+m_name = 'Bin'
+l_name = 'Noor'
+
+full_name = f_name + ' ['+ m_name +'] ' + l_name
+
+print(full_name)
